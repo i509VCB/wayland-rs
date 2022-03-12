692be54a3e8d4feea6f6ac96373a54b7830f81d8
i509VCB <i509VCB@users.noreply.github.com>
2022-03-12 05:26:45 +0000

    deploy: 244f4e6b1bc9d2aec176cb8c4f4d1e8fdf99d6e0

---
+++ wayland_protocols/staging/ext_session_lock/v1/server/ext_session_lock_surface_v1/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"constant":[["EVT_CONFIGURE_SINCE","The minimal object version supporting this event"],["REQ_ACK_CONFIGURE_SINCE","The minimal object version supporting this request"],["REQ_DESTROY_SINCE","The minimal object version supporting this request"]],"enum":[["Error",""],["Event",""],["Request",""]],"struct":[["ExtSessionLockSurfaceV1",""]]});
